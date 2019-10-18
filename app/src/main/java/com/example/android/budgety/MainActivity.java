864e2af7825d12efe537449b52604af4d32b16c6
AbdulrhmanJo <Aalhani19@gmail.com>
2019-10-18 17:43:08 +0300

    add logo img to mipmap folder

--- app/src/main/java/com/example/android/budgety/MainActivity.java
+++ app/src/main/java/com/example/android/budgety/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView elements;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        elements = (TextView) findViewById(R.id.elements);
+
+        String [] s = {"hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it"};
+
+
+        for (String ss: s) {
+            elements.append("\n\n"+ss);
+        }
+
+
     }
 
 //    public boolean onCreateOptionsMenu(Menu menu) {
