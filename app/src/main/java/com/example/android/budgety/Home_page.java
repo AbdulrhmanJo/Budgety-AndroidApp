8e9a98b179b076941ea43d7dc8a3ad790080f97b
AbdulrhmanJo <Aalhani19@gmail.com>
2019-10-19 01:16:00 +0300

    add transaction card

--- app/src/main/java/com/example/android/budgety/Home_page.java
+++ app/src/main/java/com/example/android/budgety/Home_page.java
@@ -1,13 +1,17 @@
 package com.example.android.budgety;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.autofill.AutofillValue;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.google.android.material.card.MaterialCardView;
 
@@ -27,13 +31,48 @@ public class Home_page extends AppCompatActivity {
                 "Keep up the good work", "you can do it"};
 
 
-        for (int i = 0; i <20 ; i++) {
-            MaterialCardView b = new MaterialCardView(this);
-            TextView v = new TextView(this);
-            v.setText("Hello");
-            v.setPadding(50,50,50,50);
-            b.addView(v);
-            container.addView(b);
+
+        for (int i = 0; i <s.length ; i++) {
+            MaterialCardView card = new MaterialCardView(this);
+
+
+            // Set the CardView layoutParams
+            LayoutParams params = new LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT
+            );
+
+            params.setMargins(0,15,0,20);
+            card.setLayoutParams(params);
+
+            // Set CardView corner radius
+            card.setRadius(9);
+
+            // Set cardView content padding
+            card.setContentPadding(15, 15, 15, 15);
+
+            // Set a background color for CardView
+//            card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
+
+            // Set the CardView maximum elevation
+
+            // Set CardView elevation
+            card.setCardElevation(9);
+
+            // Initialize a new TextView to put in CardView
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(params);
+            tv.setText(s[i]);
+//            tv.setTextSize(30);
+            tv.setTextColor(Color.RED);
+
+            // Put the TextView in CardView
+            card.addView(tv);
+
+            // Finally, add the CardView in root layout
+            container.addView(card);
+
+
         }
 
 
