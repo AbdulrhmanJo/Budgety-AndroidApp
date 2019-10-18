b9d2d85072c0f9f8f2efb29b4973699fe9b2232e
AbdulrhmanJo <Aalhani19@gmail.com>
2019-10-18 18:58:40 +0300

    change the start screen of the app

--- app/src/main/java/com/example/android/budgety/MainActivity.java
+++ app/src/main/java/com/example/android/budgety/MainActivity.java
@@ -15,15 +15,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         elements = (TextView) findViewById(R.id.elements);
 
-        String [] s = {"hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it","hello","hi", "Well done", "Keep up the good work", "you can do it"};
+        String[] s = {"hello", "hi", "Well done", "Keep up the good work", "you can do it",
+                "hello", "hi", "Well done", "Keep up the good work", "you can do it", "hello", "hi",
+                "Well done", "Keep up the good work", "you can do it", "hello", "hi", "Well done",
+                "Keep up the good work", "you can do it"};
 
 
-        for (String ss: s) {
-            elements.append("\n\n"+ss);
+        for (String ss : s) {
+            elements.append("\n\n" + ss);
         }
 
 
