0eec1f9ff49657fe60520974853f601e213e671b
AbdulrhmanJo <Aalhani19@gmail.com>
2019-10-19 05:01:14 +0300

    change the padding of edit text and add some changes to transaction item

--- app/src/main/java/com/example/android/budgety/Home_page.java
+++ app/src/main/java/com/example/android/budgety/Home_page.java
@@ -1,12 +1,17 @@
 package com.example.android.budgety;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.autofill.AutofillValue;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
@@ -18,6 +23,7 @@ import com.google.android.material.card.MaterialCardView;
 public class Home_page extends AppCompatActivity {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,14 +48,21 @@ public class Home_page extends AppCompatActivity {
                     LayoutParams.WRAP_CONTENT
             );
 
+            LayoutParams params2 = new LayoutParams(
+                    LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT
+            );
+
+            params2.setMargins(0,0,10,0);
             params.setMargins(0,15,0,20);
+
             card.setLayoutParams(params);
 
             // Set CardView corner radius
             card.setRadius(9);
 
             // Set cardView content padding
-            card.setContentPadding(15, 15, 15, 15);
+            card.setContentPadding(50, 50, 50, 50);
 
             // Set a background color for CardView
 //            card.setCardBackgroundColor(Color.parseColor("#FFC6D6C3"));
@@ -57,17 +70,30 @@ public class Home_page extends AppCompatActivity {
             // Set the CardView maximum elevation
 
             // Set CardView elevation
-            card.setCardElevation(9);
+            card.setCardElevation(5);
 
             // Initialize a new TextView to put in CardView
+            LinearLayout linearLayout = new LinearLayout(this);
+
+            ImageView img = new ImageView(this);
+//            img.setLayoutParams(params2);
+            img.setImageResource(R.drawable.round_keyboard_arrow_up_24);
+            img.setScaleX(1.6f);
+            img.setScaleY(1.6f);
+            img.setImageTintList(ColorStateList.valueOf(Color.RED));
             TextView tv = new TextView(this);
-            tv.setLayoutParams(params);
+
             tv.setText(s[i]);
 //            tv.setTextSize(30);
             tv.setTextColor(Color.RED);
 
+            linearLayout.addView(img);
+            linearLayout.addView(tv);
+
+
+
             // Put the TextView in CardView
-            card.addView(tv);
+            card.addView(linearLayout);
 
             // Finally, add the CardView in root layout
             container.addView(card);
